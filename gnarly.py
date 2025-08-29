db2979d64b60668bc91960777d249d393979b4b3
Juan <henzojuan82@gmail.com>
2025-08-29 20:14:29 -0300

    Add files via upload

---
+++ gnarly.py
@@ -0,0 +1,52 @@
+
+answer_user = input("They could describe everything with one single word, You know? ")
+answer_user = "Like".lower()
+
+
+print("\n")
+# Pergunta 1
+print("Boba tea")
+answer_1 = input("")
+
+answer_1 == "GNARLY".lower()
+print("")
+
+# Pergunta 2
+print("Tesla")
+answer_2 = input("")
+
+answer_2 == "GNARLY".lower()
+print("")
+
+# Pergunta 3
+print("Fried Chicken")
+answer_3 = input("")
+
+answer_3 == "GNARLY".lower()
+print("")
+
+# Pergunta 4
+print("Partyin' at the Hollywood Hills")
+answer_4 = input("")
+
+answer_4 == "GNARLY".lower()
+print("")
+
+print("This song")
+answer_4 = input("")
+
+answer_4 == "GNARLY".lower()
+print("")
+
+print("Oh my God the new Beat")
+answer_4 = input("")
+
+answer_4 == "FUCKING GNARLY".lower()
+print("")
+
+print("Oh my god is this reeeeeeal?")
+answer_4 = input("")
+
+answer_4 == "Everything is gnarly".lower()
+print("")
+   
